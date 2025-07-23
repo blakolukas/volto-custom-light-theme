9dac61da3350e606618a10d2814ebf55f5e16dc0
Samoel <samoel-silva@procergs.rs.gov.br>
2025-07-23 11:29:59 -0300

    Fix componente SiteMap para rodapé

--- packages/volto-custom-light-theme/src/components/SiteMapFooter/SiteMapFooter.jsx
+++ packages/volto-custom-light-theme/src/components/SiteMapFooter/SiteMapFooter.jsx
@@ -1,131 +1,81 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { asyncConnect } from '@plone/volto/helpers';
-import { defineMessages, injectIntl } from 'react-intl';
+import { injectIntl } from 'react-intl';
 import { Container as SemanticContainer } from 'semantic-ui-react';
-import { Helmet, toBackendLang } from '@plone/volto/helpers';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import config from '@plone/volto/registry';
+import { getNavigation } from '@plone/volto/actions';
+import { toBackendLang } from '@plone/volto/helpers';
+
 import './SiteMapFooter.css';
 
-import { getNavigation } from '@plone/volto/actions';
-
-const messages = defineMessages({
-  Sitemap: {
-    id: 'Sitemap',
-    defaultMessage: 'Sitemap',
-  },
-});
-
-export function getSitemapPath(pathname = '', lang) {
+function getSitemapPath(pathname = '', lang) {
   const prefix = pathname.replace(/\/sitemap$/gm, '').replace(/^\//, '');
   const path = prefix || lang || '';
   return path;
 }
 
-/**
- * Sitemap function component.
- * @function Sitemap
- * @param {Object} props - Component properties.
- * @returns {JSX.Element} - Rendered component.
- */
-function Sitemap(props) {
-  const {
-    location: { pathname },
-    lang,
-    getNavigation,
-  } = props;
+function SitemapFooter({ items, lang, getNavigation, intl }) {
+  const location = useLocation();
 
   useEffect(() => {
     const { settings } = config;
-    const language = settings.isMultilingual ? `${toBackendLang(lang)}` : null;
-    const path = getSitemapPath(pathname, language);
+    const language = settings.isMultilingual ? toBackendLang(lang) : null;
+    const path = getSitemapPath(location.pathname, language);
     getNavigation(path, 4);
-  }, [pathname, lang, getNavigation]);
+  }, [location.pathname, lang, getNavigation]);
 
-  const renderItems = (items) => {
-    return (
-      <ul className="rodape__mapa-site">
-        {items.map((item) => (
-          <React.Fragment key={item.title}>
-            {item.items.length > 0 && (
-              <li className="rodape__mapa-site__item" key={item.title}>
-                <Link to={item.url} className="rodape-titulo">
-                  {item.title}
-                </Link>
-                <ul>
-                  {item.items &&
-                    item.items.map((innerItem) => (
-                      <li key={innerItem.title}>
-                        <Link to={innerItem.url}>{innerItem.title}</Link>
-                      </li>
-                    ))}
-                </ul>
-              </li>
-            )}
-          </React.Fragment>
-        ))}
-      </ul>
-    );
-  };
+  const renderItems = (items) => (
+    <ul className="rodape__mapa-site">
+      {items.map((item) => (
+        <React.Fragment key={item.title}>
+          {item.items.length > 0 && (
+            <li className="rodape__mapa-site__item">
+              <Link to={item.url} className="rodape-titulo">
+                {item.title}
+              </Link>
+              <ul>
+                {item.items.map((innerItem) => (
+                  <li key={innerItem.title}>
+                    <Link to={innerItem.url}>{innerItem.title}</Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          )}
+        </React.Fragment>
+      ))}
+    </ul>
+  );
 
   const Container =
     config.getComponent({ name: 'Container' }).component || SemanticContainer;
 
   return (
-    <div>
-      <Helmet title={props.intl.formatMessage(messages.Sitemap)} />
-      <Container className="view-wrapper">
-        {props.items && renderItems(props.items, 2)}
-      </Container>
-    </div>
+    <Container className="view-wrapper">
+      {items && renderItems(items)}
+    </Container>
   );
 }
 
-Sitemap.propTypes = {
+SitemapFooter.propTypes = {
   getNavigation: PropTypes.func.isRequired,
-  location: PropTypes.object.isRequired,
-  intl: PropTypes.object.isRequired,
+  items: PropTypes.array,
   lang: PropTypes.string.isRequired,
-  items: PropTypes.array.isRequired,
+  intl: PropTypes.object.isRequired,
 };
 
-export const __test__ = compose(
-  injectIntl,
-  connect(
-    (state) => ({
-      items: state.navigation.items,
-      lang: state.intl.locale,
-    }),
-    { getNavigation },
-  ),
-)(Sitemap);
+SitemapFooter.defaultProps = {
+  items: [],
+};
 
-export default compose(
-  injectIntl,
+export default injectIntl(
   connect(
     (state) => ({
       items: state.navigation.items,
       lang: state.intl.locale,
     }),
     { getNavigation },
-  ),
-  asyncConnect([
-    {
-      key: 'navigation',
-      promise: ({ location, store: { dispatch, getState } }) => {
-        if (!__SERVER__) return;
-        const { settings } = config;
-        const path = getSitemapPath(
-          location.pathname,
-          settings.isMultilingual
-            ? toBackendLang(getState().intl.locale)
-            : null,
-        );
-        return dispatch(getNavigation(path, 4));
-      },
-    },
-  ]),
-)(Sitemap);
+  )(SitemapFooter),
+);
